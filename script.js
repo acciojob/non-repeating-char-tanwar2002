4a030b7e3f410de51c9856b3b5230d3f30072bdd
tanwar2002 <adityatanwar2002@gmail.com>
2024-12-27 15:26:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let map= new Map()
+	for(let c of str){
+		map.set(c,(map.get(c)||0)+1)
+	}
+	for(let c of str){
+		if(map.get(c)==1){
+			return c
+		}
+	}
+	return null
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
